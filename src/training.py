61d4b1949841e89312898f0cb10e9fa78db02127
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2023-05-23 20:42:43 -0400

    implementing  prepare_data() in training.py

--- src/training.py
+++ src/training.py
@@ -19,8 +19,39 @@ from stardist.models import Config2D, StarDist2D, StarDistData2D
 np.random.seed(42)
 lbl_cmap = random_label_cmap()
 
+def prepare_data():
+    X = sorted(glob('data/images/*.tif'))
+    Y = sorted(glob('data/masks/*.tif'))
+    assert all(Path(x).name == Path(y).name for x, y in zip(X, Y))
+
+    X = list(map(imread, X))
+    Y = list(map(imread, Y))
+    n_channel = 1 if X[0].ndim == 2 else X[0].shape[-1]
+
+    axis_norm = (0, 1)  # normalize channels independently
+    # axis_norm = (0,1,2) # normalize channels jointly
+    if n_channel > 1:
+        print(
+            "Normalizing image channels %s." % ('jointly' if axis_norm is None or 2 in axis_norm else 'independently'))
+        sys.stdout.flush()
+
+    X = [normalize(x, 1, 99.8, axis=axis_norm) for x in tqdm(X)]
+    Y = [fill_label_holes(y) for y in tqdm(Y)]
+
+    assert len(X) > 1, "not enough training data"
+    rng = np.random.RandomState(42)
+    ind = rng.permutation(len(X))
+    n_val = max(1, int(round(0.15 * len(ind))))
+    ind_train, ind_val = ind[:-n_val], ind[-n_val:]
+    X_val, Y_val = [X[i] for i in ind_val], [Y[i] for i in ind_val]
+    X_trn, Y_trn = [X[i] for i in ind_train], [Y[i] for i in ind_train]
+    print('number of images: %3d' % len(X))
+    print('- training:       %3d' % len(X_trn))
+    print('- validation:     %3d' % len(X_val))
+
+def main():
+    prepare_data()
 
 
-
-
-
+if __name__ == '__main__':
+    main()
