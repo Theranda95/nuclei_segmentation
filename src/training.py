45767392d5c42f61d97a7f57d06bec719fd76d0d
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2023-05-23 20:22:07 -0400

    Start of training

---
+++ src/training.py
@@ -0,0 +1,26 @@
+from __future__ import print_function, unicode_literals, absolute_import, division
+import sys
+import numpy as np
+import matplotlib
+# matplotlib.rcParams["image.interpolation"] = None
+import matplotlib.pyplot as plt
+# %matplotlib inline
+# %config InlineBackend.figure_format = 'retina'
+
+from glob import glob
+from tqdm import tqdm
+from tifffile import imread
+from csbdeep.utils import Path, normalize
+
+from stardist import fill_label_holes, random_label_cmap, calculate_extents, gputools_available
+from stardist.matching import matching, matching_dataset
+from stardist.models import Config2D, StarDist2D, StarDistData2D
+
+np.random.seed(42)
+lbl_cmap = random_label_cmap()
+
+
+
+
+
+
