24815bb393b7094d1ef505af476a835a16483f68
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2023-05-23 21:06:35 -0400

    Loading model

--- src/training.py
+++ src/training.py
@@ -19,6 +19,18 @@ from stardist.models import Config2D, StarDist2D, StarDistData2D
 np.random.seed(42)
 lbl_cmap = random_label_cmap()
 
+
+def plot_img_label(img, lbl, img_title="image", lbl_title="label", **kwargs):
+    fig, (ai,al) = plt.subplots(1,2, figsize=(12,5), gridspec_kw=dict(width_ratios=(1.25,1)))
+    im = ai.imshow(img, cmap='gray', clim=(0,1))
+    ai.set_title(img_title)
+    fig.colorbar(im, ax=ai)
+    al.imshow(lbl, cmap=lbl_cmap)
+    al.set_title(lbl_title)
+    plt.tight_layout()
+    plt.savefig('image_for_training_sample')
+
+
 def prepare_data():
     X = sorted(glob('data/images/*.tif'))
     Y = sorted(glob('data/masks/*.tif'))
@@ -49,9 +61,52 @@ def prepare_data():
     print('- training:       %3d' % len(X_trn))
     print('- validation:     %3d' % len(X_val))
 
+
+    i = min(9, len(X) - 1)
+    img, lbl = X[i], Y[i]
+    assert img.ndim in (2, 3)
+    img = img if (img.ndim == 2 or img.shape[-1] == 3) else img[..., 0]
+    plot_img_label(img, lbl)
+    None;
+
+    # 32 is a good default choice (see 1_data.ipynb)
+    n_rays = 32
+
+    # Use OpenCL-based computations for data generator during training (requires 'gputools')
+    use_gpu = False and gputools_available()
+
+    # Predict on subsampled grid for increased efficiency and larger field of view
+    grid = (2, 2)
+
+    conf = Config2D(
+        n_rays=n_rays,
+        grid=grid,
+        use_gpu=use_gpu,
+        n_channel_in=n_channel,
+    )
+    print(conf)
+    vars(conf)
+
+    if use_gpu:
+        from csbdeep.utils.tf import limit_gpu_memory
+        # adjust as necessary: limit GPU memory to be used by TensorFlow to leave some to OpenCL-based computations
+        limit_gpu_memory(0.8)
+        # alternatively, try this:
+        # limit_gpu_memory(None, allow_growth=True)
+
+    model = StarDist2D(conf, name='stardist', basedir='models')
+
+    median_size = calculate_extents(list(Y), np.median)
+    fov = np.array(model._axes_tile_overlap('YX'))
+    print(f"median object size:      {median_size}")
+    print(f"network field of view :  {fov}")
+    if any(median_size > fov):
+        print("WARNING: median object size larger than field of view of the neural network.")
+
+
 def main():
     prepare_data()
-
+    # print(Config2D.__doc__)
 
 if __name__ == '__main__':
     main()
