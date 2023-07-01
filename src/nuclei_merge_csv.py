4eb1914e01ad3c5199910cebfc6e5f2ef5f05365
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2023-06-30 22:41:22 -0400

    Done with replicate 1 and 2

--- src/nuclei_merge_csv.py
+++ src/nuclei_merge_csv.py
@@ -2,8 +2,8 @@ import pandas as pd
 import os
 from natsort import os_sorted
 
-def merge_csv_files():
-    data_dir = 'replicate2_nuclei_stacks/roi_measurements'
+
+def merge_csv_files(data_dir, output_dir, nuclei_desc_name):
     csv_files = os_sorted(os.listdir(data_dir))
     dfs = []
     cols = ['genotype', 'image', 'Area', 'Circ.', 'Solidity', 'Round']
@@ -15,12 +15,39 @@ def merge_csv_files():
         dfs.append(df)
 
     df_all = pd.concat(dfs, axis=0)
-    df_all.to_csv(os.path.join(data_dir, 'nuclei_desc.csv'), index=None)
+    df_all.to_csv(os.path.join(output_dir, nuclei_desc_name), index=None)
     print('eof')
 
 
+def compute_means(output_dir, nuclei_desc_name, nuclei_mean_name):
+    df = pd.read_csv(os.path.join(output_dir, nuclei_desc_name))
+    mean_area = df.pivot_table(index=['genotype', 'image'], values='Area', aggfunc='mean')
+    mean_circ = df.pivot_table(index=['genotype', 'image'], values='Circ.', aggfunc='mean')
+    mean_solidity = df.pivot_table(index=['genotype', 'image'], values='Solidity', aggfunc='mean')
+    mean_round = df.pivot_table(index=['genotype', 'image'], values='Round', aggfunc='mean')
+    mean_all = pd.concat([mean_area, mean_circ, mean_solidity, mean_round], axis=1)
+    mean_all.to_csv(os.path.join(output_dir, nuclei_mean_name))
+
+
 def main():
-    merge_csv_files()
+    nuclei_desc_name = 'nuclei_desc.csv'
+    nuclei_mean_name = 'nuclei_mean.csv'
+
+    # -------------------
+    # replicate2
+    # -------------------
+    data_dir = 'replicate2_nuclei_stacks/roi_measurements'
+    output_dir = os.path.dirname(data_dir)
+    # merge_csv_files(data_dir, output_dir, nuclei_desc_name)
+    # compute_means(output_dir, nuclei_desc_name, nuclei_mean_name)
+
+    # -------------------
+    # replicate1
+    # -------------------
+    data_dir = 'replicate1_nuclei_stacks/roi_measurements'
+    output_dir = os.path.dirname(data_dir)
+    merge_csv_files(data_dir, output_dir, nuclei_desc_name)
+    compute_means(output_dir, nuclei_desc_name, nuclei_mean_name)
 
 
 if __name__ == '__main__':
