f26ed03c6633c2c04120714b1184b87a70c64c34
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2023-06-30 21:40:25 -0400

    Implemented merge_csv_files

--- src/nuclei_merge_csv.py
+++ src/nuclei_merge_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 from natsort import os_sorted
 
-def main():
+def merge_csv_files():
     data_dir = 'replicate2_nuclei_stacks/roi_measurements'
     csv_files = os_sorted(os.listdir(data_dir))
     dfs = []
@@ -17,6 +17,10 @@ def main():
     df_all = pd.concat(dfs, axis=0)
     df_all.to_csv(os.path.join(data_dir, 'nuclei_desc.csv'), index=None)
     print('eof')
+
+
+def main():
+    merge_csv_files()
 
 
 if __name__ == '__main__':
